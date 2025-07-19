c6e53e8a0a851da0491f31411bb571c141c2ece6
Vasu Bansal <vasubansal1998@gmail.com>
2025-07-19 19:50:14 +0530

    [Vasu] added methods to create sockets for port 67 and 68

--- main.go
+++ main.go
@@ -2,7 +2,37 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
+
+func createUDPSendSocket() (*net.UDPConn, error) {
+	laddr, err := net.ResolveUDPAddr("udp", ":68")
+	raddr, err := net.ResolveUDPAddr("udp", "255.255.255.255:67")
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := net.DialUDP("udp", laddr, raddr)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func createUDPReceiveSocket() (*net.UDPConn, error) {
+	addr, err := net.ResolveUDPAddr("udp", ":68")
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
 
 func main() {
 	fmt.Println("DHCP client starting...")
